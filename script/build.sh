6d3bf22776485f12a89dcca7f51662637659e40e
juanmesa2097 <juanmesa2097@gmail.com>
2022-04-26 20:06:50 -0500

    Update worflow to zip assets

--- script/build.sh
+++ script/build.sh
@@ -2,21 +2,19 @@
 
 package=github.com/Ekisa-Team/ekisa-chatbots-cli
 
-package_split=(${package//\// })
-package_name=${package_split[-1]}
-
 platforms=("windows/amd64" "windows/386")
 
 for platform in "${platforms[@]}"; do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=$package_name'-'$GOOS'-'$GOARCH
+    output_name=ec'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$output_name $package
+    path=bin/$output_name
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $path $package
     if [ $? -ne 0 ]; then
         echo 'An error has ocurred. Aborting the script execution...'
         exit 1
