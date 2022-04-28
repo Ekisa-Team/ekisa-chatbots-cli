7b15102084d389637c6ac94d579a41747c5d43a1
juanmesa2097 <juanmesa2097@gmail.com>
2022-04-27 21:37:36 -0500

    Update `build.sh` and release action

--- script/build.sh
+++ script/build.sh
@@ -7,7 +7,7 @@ for platform in "${platforms[@]}"; do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=ec'-'$GOOS'-'$GOARCH
+    output_name=kibot'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
